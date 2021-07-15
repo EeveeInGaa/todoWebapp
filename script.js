ac2706e29661907291b48467cc04fb46562e6c6c
Inga <inga.s6699@gmail.com>
2021-07-15 14:38:01 +0200

    add: striked todo when checkbox is checked

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ addButton.addEventListener("click", addTodoToList);
 const listOfTodos = document.querySelector("#todo-list");
 let todoArr = [];
 
+const text = undefined;
+
 //add todo to list using the 'add it' button
 function addTodoToList() {
     //create list element
     const createListElement = document.createElement("li");
+    createListElement.classList.add("listElement");
     listOfTodos.appendChild(createListElement);
     
     //create checkbox
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+    checkbox.classList.add("checkbox");
     createListElement.appendChild(checkbox);
 
     //get user input in list and clear text after user input
@@ -22,4 +26,13 @@ function addTodoToList() {
     todoArr.push(text);
     document.querySelector("#input-todo").value = "";
 
+    //strike todos when checkbox is checked
+    checkbox.addEventListener("click", function() {
+        if(createListElement.className != "checked") {
+            createListElement.className = "checked";
+        } else {
+            createListElement.className = "";
+        }
+    })
 }
+
