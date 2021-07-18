7c8f5ad790834a2fa587c2d8386366bbee0dea7c
Inga <inga.s6699@gmail.com>
2021-07-18 21:39:28 +0200

    fix: reordered elements for better readability and for logical reasons

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-const addButton = document.querySelector("#add-button");
-addButton.addEventListener("click", addTodoToList);
-
-const listOfTodos = document.querySelector("#todo-list");
 let todoArr = [];
+
+const addButton = document.querySelector("#add-button");
+const inputTodo = document.querySelector("#input-todo")
+const listOfTodos = document.querySelector("#todo-list");
+
+addButton.addEventListener("click", addTodoToList);
 
 //add todo to list using the 'add it' button
 function addTodoToList() {
+    const text = inputTodo.value;
+    //get user input in list and clear text after user input
+    const textNode = document.createTextNode(text);
+    todoArr.push(text);
+    inputTodo.value = "";
+
     //create list element
     const createListElement = document.createElement("li");
     createListElement.classList.add("list-element");
-    listOfTodos.appendChild(createListElement);
     
     //create checkbox
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.classList.add("checkbox");
-    createListElement.appendChild(checkbox);
-
-    //get user input in list and clear text after user input
-    const text = document.querySelector("#input-todo").value;
-    const textNode = document.createTextNode(text);
-    createListElement.appendChild(textNode);
-    todoArr.push(text);
-    document.querySelector("#input-todo").value = "";
-
+    
+    
     //strike todos when checkbox is checked
-    checkbox.addEventListener("click", function() {
+    checkbox.addEventListener("change", function() {
         if(createListElement.className != "checked") {
             createListElement.className = "checked";
         } else {
             createListElement.className = "";
         }
     })
+
+    listOfTodos.appendChild(createListElement);
+    createListElement.appendChild(checkbox);
+    createListElement.appendChild(textNode);
 }
 
